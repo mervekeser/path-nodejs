9f9e83848e2358f539a48df2eb176d4a0e2db449
Merve Keser <mervekeser8@outlook.com>
2025-04-03 12:58:40 +0300

    async-await workshop

---
+++ async-js/post.js
@@ -0,0 +1,41 @@
+const posts = [
+  {
+    title: "Title 1",
+    description: "Description 1",
+  },
+];
+
+function getPost() {
+  return new Promise((resolve) => {
+    console.log("Posts");
+    posts.map((post) => {
+      console.log(post.title);
+    });
+    resolve();
+  });
+}
+
+function addPost(newPost) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(newPost);
+      resolve(posts);
+
+      //reject("an error");
+    }, 2000);
+  });
+}
+
+const newPost = { title: "Title 2", description: "Description 2" };
+
+async function showPost() {
+  try {
+    await getPost();
+    await addPost(newPost);
+    await getPost();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+showPost();
