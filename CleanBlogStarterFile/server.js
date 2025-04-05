2486c7e931272a4ff58dd8b6cd36e941fcec669b
Merve Keser <mervekeser8@outlook.com>
2025-04-05 13:46:13 +0300

    template engine

--- CleanBlogStarterFile/server.js
+++ CleanBlogStarterFile/server.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+//Routes
 app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add_post");
 });
 
 const PORT = 3000;
