463eb92f3a21462bcda49f410405f1b0ece98a16
Merve Keser <mervekeser8@outlook.com>
2025-04-07 13:11:53 +0300

    post request

--- CleanBlogStarterFile/server.js
+++ CleanBlogStarterFile/server.js
@@ -1,18 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const path = require("path");
+const Post = require("./models/Post");
 
 const app = express();
+
+//Connect DB
+mongoose.connect("mongodb://localhost/clean-blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Engine
 app.set("view engine", "ejs");
 
 //Middlewares
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {
+    posts,
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -21,6 +34,11 @@ app.get("/about", (req, res) => {
 
 app.get("/add", (req, res) => {
   res.render("add_post");
+});
+
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
 });
 
 const PORT = 3000;
